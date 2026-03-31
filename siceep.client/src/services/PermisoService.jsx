60795df9b41172a65dc1bc51171c56f169fe35c3
plpc91 <plpc91@PLPC091>
2026-03-31 16:40:54 -0600

    cambio de nombre enendpoint - permisosService

--- siceep.client/src/services/PermisoService.jsx
+++ siceep.client/src/services/PermisoService.jsx
@@ -1,6 +1,6 @@
 import api from "./../api/api";
 
 export const getPermisos = async (idUsuario) => {
-    return await api.get(`Permisos/ListarGeneral_ByUsuario?idUsuario=${idUsuario}`);
+    return await api.get(`Permisos/Listar_ByUsuario?idUsuario=${idUsuario}`);
 };
 
